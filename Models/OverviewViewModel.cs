406d3ea1eeac1ead23d2e9999c50bbe71bc08723
JoJo <unslavedcoder@gmail.com>
2020-05-08 12:29:42 +0200

    created ViewModel

---
+++ Models/OverviewViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Garage_2.Models
+{
+    public class OverviewViewModel
+    {
+        public string RegNr { get; }
+        public VehicleType VehicleType { get; }
+        public DateTime TimeOfArrival { get; }
+        
+        private string timeInGarage;
+        public string TimeInGarage
+        {
+            get
+            {
+                var timeInGarage = DateTime.Now.Subtract(TimeOfArrival);
+                return String.Format($"{timeInGarage.Hours}:{ timeInGarage.Minutes}:{timeInGarage.Seconds}");
+            }
+        }
+
+    }
+}
