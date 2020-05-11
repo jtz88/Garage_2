91479c3aec56fcb4bdf5ae5b0c71cb43d1a5791f
JoJo <unslavedcoder@gmail.com>
2020-05-11 19:59:15 +0200

    added vehicles to seed

--- Data/Garage_2Context.cs
+++ Data/Garage_2Context.cs
@@ -27,7 +27,11 @@ namespace Garage_2.Data
 
             modelBuilder.Entity<ParkedVehicle>()
                 .HasData(
-                  new ParkedVehicle { Id = 1, RegNr = "ABC123", Color = "Blue", Brand = "Volkswagen", Model = "Sedan", vehicleType=VehicleType.Car, NrOfWheels=4 }
+                  new ParkedVehicle { Id = 1, RegNr = "ABC123", Color = "Blue", Brand = "Volkswagen", Model = "Sedan", vehicleType=VehicleType.Car, NrOfWheels=4 },
+                  new ParkedVehicle { Id = 2, RegNr = "EFG456", Color = "White", Brand = "Boeing", Model = "747", vehicleType = VehicleType.Airplane, NrOfWheels = 8 },
+                  new ParkedVehicle { Id = 3, RegNr = "JKL789", Color = "Gold", Brand = "Honda", Model = "Goldwing", vehicleType = VehicleType.Motorcycle, NrOfWheels = 2 },
+                  new ParkedVehicle { Id = 4, RegNr = "GHJ730", Color = "Yellow", Brand = "Coronet", Model = "32C", vehicleType = VehicleType.Boat, NrOfWheels = 0 },
+                  new ParkedVehicle { Id = 5, RegNr = "IOP466", Color = "Red", Brand = "Volvo", Model = "Single-Decker", vehicleType = VehicleType.Bus, NrOfWheels = 12 }
                  );
         }
     }
---
+++ Migrations/20200511153737_changes.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Garage_2.Migrations
+{
+    public partial class changes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "TimeOfArrival",
+                table: "ParkedVehicle",
+                nullable: false,
+                defaultValueSql: "getdate()",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2020, 5, 11, 15, 51, 54, 445, DateTimeKind.Local).AddTicks(4328));
+
+            migrationBuilder.InsertData(
+                table: "ParkedVehicle",
+                columns: new[] { "Id", "Brand", "Color", "Model", "NrOfWheels", "RegNr", "vehicleType" },
+                values: new object[,]
+                {
+                    { 2, "Boeing", "White", "747", 8, "EFG456", 0 },
+                    { 3, "Honda", "Gold", "Goldwing", 2, "JKL789", 4 },
+                    { 4, "Coronet", "Yellow", "32C", 0, "GHJ730", 1 },
+                    { 5, "Volvo", "Red", "Single-Decker", 12, "IOP466", 2 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "ParkedVehicle",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "ParkedVehicle",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "ParkedVehicle",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "ParkedVehicle",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "TimeOfArrival",
+                table: "ParkedVehicle",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2020, 5, 11, 15, 51, 54, 445, DateTimeKind.Local).AddTicks(4328),
+                oldClrType: typeof(DateTime),
+                oldDefaultValueSql: "getdate()");
+        }
+    }
+}
