3a17c8db000c7ceffa20dc166c29126fdaa82b42
paz.lindberg@gmail.com <paz.lindberg@gmail.com>
2020-05-11 15:01:36 +0200

    kvitto 3.1

--- Controllers/ParkedVehiclesController.cs
+++ Controllers/ParkedVehiclesController.cs
@@ -199,7 +199,7 @@ namespace Garage_2.Controllers
             return View(parkedVehicle);
         }
 
-        public IActionResult Receipt(string regnr, VehicleType vehicleType, int nrOfWheels, string color, string brand, string model, DateTime timeOfArrival, DateTime timeInGarage)
+        public IActionResult Receipt(string regnr, VehicleType vehicleType, int nrOfWheels, string color, string brand, string model, DateTime timeOfArrival, string timeInGarage)
         {
             //https://localhost:44347/ParkedVehicles/Receipt?regnr=aaa&vehicleType=2&nrOfWheels=3&color=red&brand=ccc&model=ddd&timeOfArrival=2000-01-01
             ViewData["regnr"] = regnr;
@@ -213,7 +213,7 @@ namespace Garage_2.Controllers
             return View();
         }
         
-        public IActionResult AskReceipt(string regnr, VehicleType vehicleType, int nrOfWheels, string color, string brand, string model, DateTime timeOfArrival, DateTime timeInGarage)
+        public IActionResult AskReceipt(string regnr, VehicleType vehicleType, int nrOfWheels, string color, string brand, string model, DateTime timeOfArrival, string timeInGarage)
         {
             ViewData["regnr"] = regnr;
             ViewData["vehicleType"] = vehicleType;
@@ -258,8 +258,8 @@ namespace Garage_2.Controllers
             var b = parkedVehicle.Brand;
             var m = parkedVehicle.Model;            //whatever
             var t = parkedVehicle.TimeOfArrival;
-            //var tg = parkedVehicle.TimeInGarage;
-            var tg = DateTime.Now.Subtract(t);
+            var tg = parkedVehicle.TimeInGarage;
+            //var tg = DateTime.Now.Subtract(t);
             var routeValues = new RouteValueDictionary  {
                 { "regnr", r },
                 { "vehicleType", v },
